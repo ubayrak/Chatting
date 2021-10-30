5e113e6ffaf655495c33ed79654ed9633575b221
Ugur Bayrak <ubayrak@protonmail.com>
2021-10-30 13:36:50 +0300

    improving func rx-tx

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 
-host = '10.96.7.207'
+host = '10.96.4.57'
 port = 2323
 FORMAT = 'utf-8'
 EXIT = "exit"
@@ -9,31 +9,21 @@ AD = (host, port)
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c.connect(AD)
 
+
 s_message = c.recv(1024).decode(FORMAT)
 print(s_message)
 
 
-# def send(msg):
-# 	message = msg.encode(FORMAT)
-# 	c.send(message)
-
-while True:
-	mesaj = input()
-	emesaj = mesaj.encode(FORMAT)
-	c.send(emesaj)
+def tx(conn):
+    mesaj = input()
+    conn.send(mesaj.encode(FORMAT))
+    return mesaj
 
 
+connection = True
+while connection:
+	tx_message = tx(c)
+	if tx_message == EXIT:
+		connection = False
 
-# send("Hello World!")
-# input()
-# send(EXIT)
-# s_message = c.recv(1024).decode(FORMAT)
-# print(s_message)
-
-
-
-
-
-
-
-
+c.close
--- server.py
+++ server.py
@@ -1,32 +1,42 @@
 import socket
 
-host = '10.96.7.207'
+host = '10.96.4.57'
 port = 2323
 FORMAT = 'utf-8'
 EXIT = 'exit'
 AD = (host, port)
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # socket created with IPv4 and TCP features
 s.bind(AD)
 
 
-def chat(conn, addr):
-    connected = True
-    while connected:
-        msg = conn.recv(1024).decode(FORMAT)
-        print(f"[{addr}] : {msg}")
+def rx(conn, addr):
+    msg = conn.recv(1024).decode(FORMAT)
+    print(f"[{addr}] : {msg}")
+    return msg
 
-        if msg == EXIT:
-        	connected = False
 
-    conn.close()
-        
+def tx(conn):
+    mesaj = input()
+    conn.send(mesaj.encode(FORMAT))
+
+
+#Program Starts
 s.listen()
 print(f"[LISTENING] Server is listening on {host}:{port}")
-
+#Connection acceptance
 client, addr = s.accept()
 print(f"[NEW CONNECTION] {addr} connected.")
-client.send("WELCOME MAN!!! PLEASE WRITE SOMeTHING".encode(FORMAT))
-chat(client, addr)
+client.send("Welcome to the session".encode(FORMAT))
 
+
+connection = True
+while connection:
+    msg = rx(client, addr)
+    if msg == EXIT:
+        connection = False
+    
+
+client.close()
